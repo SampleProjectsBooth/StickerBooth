5105894de1278caa07f38b5d78fdfa708bd214de
djr <jiaruiding@qq.com>
2020-03-03 11:27:58 +0800

    - 优化内存

--- StickerBooth/Class/Utility/JRPHAssetManager.m
+++ StickerBooth/Class/Utility/JRPHAssetManager.m
@@ -11,58 +11,37 @@
 
 @interface JRPHAssetManager ()
 
-@property (strong, nonatomic) dispatch_queue_t queue;
-
 @end
 
 @implementation JRPHAssetManager
 
-static JRPHAssetManager *_manager = nil;
 
-- (instancetype)init
+
++ (NSArray <PHAsset *>*)jr_GetAllPhotos
 {
-    self = [super init];
-    if (self) {
-        _queue = dispatch_queue_create("com.jr.phasset", DISPATCH_QUEUE_CONCURRENT);
-    } return self;
-}
-
-
-+ (void)jr_GetAllPhotos:(void(^)(NSArray <PHAsset *>*photos))completeBlock
-{
-    if (!_manager) {
-        _manager = [[JRPHAssetManager alloc] init];
-    }
-    
-    dispatch_async(_manager.queue, ^{
-        NSMutableArray *stickers = [NSMutableArray arrayWithCapacity:1];
-        if (@available(iOS 8.0, *)){
-            PHFetchOptions *option = [[PHFetchOptions alloc] init];
-            option.predicate = [NSPredicate predicateWithFormat:@"mediaType == %ld", PHAssetMediaTypeImage];
-            option.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO]];
-            PHFetchResult *smartAlbums = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeSmartAlbumUserLibrary options:nil];
-            
-            PHFetchResult *fetchResult = nil;
-            for (PHAssetCollection *collection in smartAlbums) {
-                // 有可能是PHCollectionList类的的对象，过滤掉
-                if (![collection isKindOfClass:[PHAssetCollection class]]) continue;
-                fetchResult = [PHAsset fetchAssetsInAssetCollection:collection options:option];
-                break;
-            }
-            
-            for (PHAsset *asset in fetchResult) {
-                [stickers addObject:asset];
-            }
+    NSMutableArray *stickers = [NSMutableArray arrayWithCapacity:1];
+    if (@available(iOS 8.0, *)){
+        PHFetchOptions *option = [[PHFetchOptions alloc] init];
+        option.predicate = [NSPredicate predicateWithFormat:@"mediaType == %ld", PHAssetMediaTypeImage];
+        option.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO]];
+        PHFetchResult *smartAlbums = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeSmartAlbumUserLibrary options:nil];
+        
+        PHFetchResult *fetchResult = nil;
+        for (PHAssetCollection *collection in smartAlbums) {
+            // 有可能是PHCollectionList类的的对象，过滤掉
+            if (![collection isKindOfClass:[PHAssetCollection class]]) continue;
+            fetchResult = [PHAsset fetchAssetsInAssetCollection:collection options:option];
+            break;
         }
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (completeBlock) {
-                completeBlock([stickers copy]);
-            }
-        });
-    });
+        
+        for (PHAsset *asset in fetchResult) {
+            [stickers addObject:asset];
+        }
+    }
+    return [stickers copy];
 }
 
-+ (PHImageRequestID)getPhotoDataWithAsset:(id)asset completion:(void (^)(NSData *data,NSDictionary *info,BOOL isDegraded))completion progressHandler:(void (^)(double progress, NSError *error, BOOL *stop, NSDictionary *info))progressHandler {
++ (PHImageRequestID)jr_GetPhotoDataWithAsset:(id)asset completion:(void (^)(NSData *data,NSDictionary *info,BOOL isDegraded))completion progressHandler:(void (^)(double progress, NSError *error, BOOL *stop, NSDictionary *info))progressHandler {
     if ([asset isKindOfClass:[PHAsset class]]) {
         BOOL isGif = [[asset valueForKey:@"uniformTypeIdentifier"] isEqualToString:(__bridge NSString*)kUTTypeGIF];
         PHImageRequestOptions *option = [[PHImageRequestOptions alloc]init]; option.resizeMode = PHImageRequestOptionsResizeModeFast;
